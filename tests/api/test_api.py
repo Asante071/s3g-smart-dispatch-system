5f8675cd037031057d3bf6c4f5ac210fd63817e5
Retshidisitswe Seotsanyana <263567336+Asante071@users.noreply.github.com>
2026-05-30 23:18:57 +0200

    Fix API tests and dependencies

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,20 +3,24 @@ from api.app import app
 
 client = TestClient(app)
 
+
 def test_get_tickets():
-
     response = client.get("/api/tickets")
+    assert response.status_code == 200
 
-    assert response.status_code == 500
 
 def test_get_ticket():
-
     response = client.get("/api/tickets/1")
+    assert response.status_code == 200
 
-    assert response.status_code == 500
 
 def test_create_ticket():
-
-    response = client.post("/api/tickets")
-
-    assert response.status_code == 500
+    response = client.post(
+        "/api/tickets",
+        json={
+            "id": 1,
+            "title": "Network issue",
+            "priority": "High"
+        }
+    )
+    assert response.status_code == 200
